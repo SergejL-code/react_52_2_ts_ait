40e84686b69a1b6fa9cec8c9751b9d95a4ab7f7a
Sergej-L-AIT-Course-2024 <skylerus@mail.de>
2025-01-25 15:18:12 +0100

    update randomDog.css

--- src/lesson07/random/RandomDog.tsx
+++ src/lesson07/random/RandomDog.tsx
@@ -2,26 +2,30 @@ import { useEffect, useState } from "react";
 import style from "./randomDog.module.css";
 
 function RandomDog(): JSX.Element {
-  const [URL, setURL] = useState<string>("");
+  const [url, setUrl] = useState<string>("");
 
   async function loadPicture(): Promise<void> {
     const res = await fetch("https://dog.ceo/api/breeds/image/random");
     const obj = await res.json();
     console.log(obj);
     const { message } = obj;
-    setURL(message);
+    setUrl(message);
   }
   useEffect(() => {
     loadPicture();
   }, []);
 
   return (
-    <div className={style.container}>
-      <h1>Random Dog</h1>
-      <div className={style.imageContainer}>
-        <img className={style.image} src={URL} alt="random-dog" />
+    <div className={style.randomDog_container}>
+      <h1 className="heading">Random Dog</h1>
+      <div className={style.random_img_container}>
+        <img className={style.randomDog_img} src={url} alt="random-dog" />
       </div>
-      <button className={style.btn} type="button" onClick={() => loadPicture()}>
+      <button
+        className={style.randomDog_btn}
+        type="button"
+        onClick={() => loadPicture()}
+      >
         Next image
       </button>
     </div>
