84f3bf726446a8b189bac7c40889f61a44d4f12a
Sergej-L-AIT-Course-2024 <skylerus@mail.de>
2025-02-06 19:57:31 +0100

    update (myInput.tsx )

--- src/components/myInput/MyInput.tsx
+++ src/components/myInput/MyInput.tsx
@@ -1,21 +1,37 @@
-import "./myInput.css";
+import { FormikProps } from "formik";
+import styles from "./myInput.module.css";
 interface IMyInput {
   name: string;
-  type: " password" | "email" | "text" | "number";
-  placeholder: string;
-  label: string;
+  type?: " password" | "email" | "text" | "number";
+  placeholder?: string;
+  label?: string;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  formik: FormikProps<any>;
 }
-function MyInput({ name, type, placeholder, label }: IMyInput): JSX.Element {
+function MyInput({
+  name,
+  type = "text",
+  placeholder = "input text",
+  label = "label text",
+  formik,
+}: IMyInput): JSX.Element {
+  const { handleChange, values, errors } = formik;
   return (
-    <>
-      <label>{label}</label>
+    <div>
+      {errors[name] ? (
+        <label className={styles.errorText}>{errors[name] as string}</label>
+      ) : (
+        <label>{label}</label>
+      )}
       <input
-        className="myInput"
+        onChange={handleChange}
+        value={values[name]}
+        className={styles.myInput}
         placeholder={placeholder}
         name={name}
         type={type}
       />
-    </>
+    </div>
   );
 }
 export default MyInput;
--- src/lessons/lesson16/Lesson16.tsx
+++ src/lessons/lesson16/Lesson16.tsx
@@ -1,20 +1,62 @@
-// import styles from './lesson16.module.css'
-
+import styles from "./lesson16.module.css";
+import * as Yup from "yup";
 import { color } from "framer-motion";
 import ChildrenProps from "../../components/childrenProps/ChildrenProps";
+import MyInput from "../../components/myInput/MyInput";
+import MyButton from "../../components/myButton/MyButton";
+import { useFormik } from "formik";
 
+const schema = Yup.object().shape({
+  email: Yup.string()
+    .email("type valid email")
+    .required("email is required field"),
+
+  password: Yup.string()
+    .min(8, "password need more than 8 symbols")
+    .required("password is required field "),
+});
 function Lesson16(): JSX.Element {
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    } as { email: string; password: string },
+    validateOnChange: false,
+    validationSchema: schema,
+    onSubmit: (values, { resetForm }) => {
+      console.log(values);
+      resetForm();
+    },
+  });
   return (
-    <div>
+    <div className={styles.lessonContainer}>
       <h2>Lesson 16: practice before test</h2>
       <ChildrenProps func={() => console.log("много разных пропсов 🙇‍♂️")}>
         {/* обернутые данные придут на место children */}
         <p style={{ color: "red" }}>переданные данные</p>
         <ul>
-          <li>данные из lesson 16</li>
-          <li>можно передать много данных</li>
+          <li style={{ listStyle: "none" }}>данные из lesson 16</li>
+          <li style={{ listStyle: "none" }}>можно передать много данных</li>
         </ul>
       </ChildrenProps>
+
+      <h2> 2. Input components </h2>
+
+      <form style={{ width: "300px" }} onSubmit={formik.handleSubmit}>
+        <MyInput
+          formik={formik}
+          name="email"
+          placeholder="email"
+          label="type your email please"
+        />
+        <MyInput
+          formik={formik}
+          name="password"
+          placeholder="password"
+          label="type your secret pass please"
+        />
+        <MyButton text="sign in" />
+      </form>
     </div>
   );
 }
